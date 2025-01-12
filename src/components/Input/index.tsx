445d8c65d1b91379af319881891d983d4a5420d4
JakeSiewJK64 <joekanesiew@outlook.com>
2025-01-12 15:41:51 +0800

    add input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,54 @@
+import { HTMLInputTypeAttribute } from "react";
+import { cn } from "../../utils";
+import "../../index.css";
+
+export const Input = ({
+  placeholder,
+  label,
+  id,
+  value,
+  errorMessage,
+  className,
+  checked = false,
+  type = "text",
+}: {
+  /** Error message (if any) */
+  errorMessage?: string;
+  /** Checked or not (applicable for checkbox input type) */
+  checked?: boolean;
+  /** Value of input */
+  value?: string | number;
+  /** Input label */
+  label?: string;
+  /** Input placeholder */
+  placeholder?: string;
+  /** Component classname */
+  className?: string;
+  /** Component id */
+  id?: string;
+  /** Input type */
+  type?: HTMLInputTypeAttribute;
+}) => {
+  return (
+    <>
+      <label className="block text-gray-700 text-sm font-bold mb-2">
+        {label}
+      </label>
+      <input
+        className={cn(
+          `shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+            errorMessage && "border-red-500"
+          } ${className}`
+        )}
+        checked={checked}
+        value={value}
+        id={id}
+        type={type}
+        placeholder={placeholder}
+      />
+      {errorMessage && (
+        <p className="text-red-500 text-xs italic">{errorMessage}</p>
+      )}
+    </>
+  );
+};
---
+++ src/stories/Input.stories.ts
@@ -0,0 +1,56 @@
+import { Meta } from "@storybook/react";
+import { HTMLInputTypeAttribute } from "react";
+import { Input } from "../components/Input";
+
+const InputTypesOptions: HTMLInputTypeAttribute[] = [
+  "button",
+  "checkbox",
+  "color",
+  "date",
+  "datetime-local",
+  "email",
+  "file",
+  "hidden",
+  "image",
+  "month",
+  "number",
+  "password",
+  "radio",
+  "range",
+  "reset",
+  "search",
+  "submit",
+  "tel",
+  "text",
+  "time",
+  "url",
+  "week",
+];
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const InputStory: Meta<typeof Input> = {
+  title: "Example/Input",
+  component: Input,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {
+    type: {
+      control: "select",
+      options: InputTypesOptions,
+    },
+  },
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Small = {
+  args: {
+    label: "Input",
+  },
+};
+
+export default InputStory;
