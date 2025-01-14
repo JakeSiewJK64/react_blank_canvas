d31216094f69c16731fb71ac36d55dd023669cb0
JakeSiewJK64 <joekanesiew@outlook.com>
2025-01-15 06:46:46 +0800

    update button story

--- src/stories/Card.stories.tsx
+++ src/stories/Card.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta, StoryFn } from "@storybook/react";
 import { Card } from "../components/Card";
+import { Button } from "../components/Button";
 
 const CardStory: Meta<typeof Card> = {
   title: "Example/Card",
@@ -13,17 +14,28 @@ const CardStory: Meta<typeof Card> = {
 
 export const Regular: StoryFn<typeof Card> = (args) => (
   <Card {...args}>
-    <span>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-      Lorem Ipsum has been the industry's standard dummy text ever since the
-      1500s, when an unknown printer took a galley of type and scrambled it to
-      make a type specimen book. It has survived not only five centuries, but
-      also the leap into electronic typesetting, remaining essentially
-      unchanged. It was popularised in the 1960s with the release of Letraset
-      sheets containing Lorem Ipsum passages, and more recently with desktop
-      publishing software like Aldus PageMaker including versions of Lorem
-      Ipsum.
-    </span>
+    <div className="w-[25rem]">
+      <img src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png" />
+      <div className="h-[10rem] overflow-y-scroll">
+        <span>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </span>
+      </div>
+    </div>
+    <div className="flex flex-row gap-1 justify-end mt-4">
+      <Button size="sm" variant="outline">
+        Cancel
+      </Button>
+      <Button size="sm">Confirm</Button>
+    </div>
   </Card>
 );
 
