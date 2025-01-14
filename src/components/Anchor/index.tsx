ad883b6e66f763bfaa4b3e2e8c59d82929d757bf
JakeSiewJK64 <joekanesiew@outlook.com>
2025-01-14 16:47:11 +0800

    add classname for anchor

--- src/components/Anchor/index.tsx
+++ src/components/Anchor/index.tsx
@@ -5,13 +5,15 @@ import "../../index.css";
 export const Anchor = ({
   href,
   children,
+  className,
 }: {
   href: string;
   children: ReactElement | string;
+  className?: string;
 }) => {
   return (
     <a
-      className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
+      className={`underline text-blue-600 hover:text-blue-800 visited:text-purple-600 ${className}`}
       href={href}
     >
       {children}
