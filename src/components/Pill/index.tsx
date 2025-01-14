ee9363d0782be450a49d9ae146ba12f039dfcac7
JakeSiewJK64 <joekanesiew@outlook.com>
2025-01-14 08:45:27 +0800

    add pill

---
+++ src/components/Pill/index.tsx
@@ -0,0 +1,59 @@
+import { ReactElement } from "react";
+import { cn } from "../../utils";
+import "../../index.css";
+
+/** Standard pill component. */
+export const Pill = ({
+  children = "Pill",
+  className,
+  size = "md",
+  color = "none",
+  withClose = false,
+  onClick = () => {},
+  onCloseClick = () => {},
+}: {
+  /** tailwind class */
+  className?: string;
+  /** children */
+  children?: ReactElement | string;
+  /** size of pill */
+  size?: "sm" | "md" | "lg" | "xl";
+  /** color of pill */
+  color?: "danger" | "warn" | "success" | "primary" | "none";
+  /** onClick callback */
+  onClick?: () => void;
+  /** include close button */
+  withClose?: boolean;
+  /** on close button click */
+  onCloseClick?: () => void;
+}) => {
+  const sizing = {
+    sm: "py-1 px-2",
+    md: "py-2 px-4",
+    lg: "py-3 px-6",
+    xl: "py-4 px-8",
+  }[size];
+  const coloring = {
+    none: "",
+    danger: "bg-red-600 text-white",
+    warn: "bg-amber-600 text-white",
+    success: "bg-green-600 text-white",
+    primary: "bg-[#09ABC2] text-white",
+  }[color];
+
+  return (
+    <div
+      onClick={onClick}
+      className={cn(
+        `bg-slate-200 border-slate-400 border-[1px] font-sans font-semibold text-sm rounded-full ${sizing} ${coloring} ${className}`
+      )}
+    >
+      {children}
+      {withClose && (
+        <button className="text-xs ms-2" onClick={onCloseClick}>
+          X
+        </button>
+      )}
+    </div>
+  );
+};
---
+++ src/stories/Pill.stories.ts
@@ -0,0 +1,55 @@
+import { Meta } from "@storybook/react";
+import { Pill } from "../components/Pill";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const PillStory: Meta<typeof Pill> = {
+  title: "Example/Pill",
+  component: Pill,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {
+    children: {
+      control: "text",
+    },
+    size: {
+      control: "select",
+      options: ["sm", "md", "lg", "xl"],
+    },
+  },
+};
+
+export const Regular = {
+  args: {
+    children: "Pill",
+  },
+};
+
+export const RegularDanger = {
+  args: {
+    color: "danger",
+  },
+};
+
+/** Small Pill */
+export const Small = {
+  args: { size: "sm" },
+};
+/** Small Pill */
+export const WithCloseButton = {
+  args: { withClose: true },
+};
+/** Large Pill */
+export const Large = {
+  args: { size: "lg" },
+};
+/** Extra large pill */
+export const ExtraLarge = {
+  args: { size: "xl" },
+};
+
+export default PillStory;
