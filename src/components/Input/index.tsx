df775b4d564534b0099dd9d3c8638579feace562
JakeSiewJK64 <joekanesiew@outlook.com>
2025-01-17 17:54:00 +0800

    update input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,62 +1,33 @@
-import { HTMLInputTypeAttribute } from "react";
+import { InputHTMLAttributes } from "react";
 import { cn } from "../../utils";
 import "../../index.css";
 
 export const Input = ({
-  placeholder,
   label,
-  id,
-  value,
   errorMessage,
-  className,
-  checked = false,
-  disabled = false,
-  type = "text",
-  onChange = () => {},
+  ...props
 }: {
   /** Error message (if any) */
   errorMessage?: string;
   /** Checked or not (applicable for checkbox input type) */
-  checked?: boolean;
-  /** disabled or not */
-  disabled?: boolean;
-  /** Value of input */
-  value?: string | number;
-  /** Input label */
   label?: string;
-  /** Input placeholder */
-  placeholder?: string;
-  /** Component classname */
-  className?: string;
-  /** Component id */
-  id?: string;
-  /** Input type */
-  type?: HTMLInputTypeAttribute;
-  /** onchange callback */
-  onChange?: (e: unknown) => void;
-}) => {
+} & InputHTMLAttributes<HTMLInputElement>) => {
   return (
-    <>
+    <div>
       <label className="block text-gray-700 text-sm font-bold mb-2">
         {label}
       </label>
       <input
-        disabled={disabled}
-        onChange={onChange}
         className={cn(
           `shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
             errorMessage && "border-red-500"
-          } ${className}`
+          } ${props.className}`
         )}
-        checked={checked}
-        value={value}
-        id={id}
-        type={type}
-        placeholder={placeholder}
+        {...props}
       />
       {errorMessage && (
         <p className="text-red-500 text-xs italic">{errorMessage}</p>
       )}
-    </>
+    </div>
   );
 };
