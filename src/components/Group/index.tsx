d1f28dd728eff483f77c188df72620bdbbe062fb
JakeSiewJK64 <joekanesiew@outlook.com>
2025-01-19 16:58:59 +0800

    update group

--- src/components/Group/index.tsx
+++ src/components/Group/index.tsx
@@ -1,17 +1,25 @@
-import { ReactNode } from "react";
+import { CSSProperties, ReactNode } from "react";
 import { cn } from "../../utils";
 
 export const Group = ({
   children,
   className,
   gap,
+  align,
+  justify,
 }: {
   gap?: number;
   children: ReactNode | ReactNode[];
+  align?: CSSProperties["alignItems"];
+  justify?: CSSProperties["justifyItems"];
   className?: string;
 }) => {
   return (
-    <div className={cn(`flex flex-row gap-${gap} ${className}`)}>
+    <div
+      className={cn(
+        `flex flex-row gap-${gap} justify-${justify} align-${align} ${className}`
+      )}
+    >
       {children}
     </div>
   );
