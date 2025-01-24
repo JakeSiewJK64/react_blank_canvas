e5bf4ed614239d91ef67072413b866335a8939f8
JakeSiewJK64 <joekanesiew@outlook.com>
2025-01-24 08:11:40 +0800

    update button story

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,6 +1,8 @@
 import Monicon from "@monicon/react";
 import { Meta } from "@storybook/react";
 import { Button } from "../components/Button";
+import { Group } from "../components/Group";
+import { Stack } from "../components/Stack";
 
 const ButtonStory: Meta<typeof Button> = {
   title: "Core/Button",
@@ -9,69 +11,97 @@ const ButtonStory: Meta<typeof Button> = {
     layout: "centered",
   },
   tags: ["autodocs"],
-  argTypes: {},
 };
 
-export const Large = {
-  args: {
-    children: "Button",
-    size: "lg",
-  },
+export const StandardButton = (args: typeof Button) => {
+  return <Button {...args}>Button</Button>;
 };
 
-export const Regular = {
-  args: {
-    color: "primary",
-    children: "Button",
-  },
-};
-
-export const Small = {
-  args: {
-    size: "sm",
-    children: "Button",
-  },
-};
-
-export const Loading = {
-  args: {
-    size: "sm",
-    loading: true,
-    children: "Button",
-  },
-};
-
-export const Elevated = {
-  args: {
-    variant: "elevated",
-    children: "Button",
-  },
-};
-
-export const Icon = {
-  args: {
-    variant: "outline",
-    color: "outline",
-    size: "md",
-    icon: <Monicon color="white" name="lucide:x" size={15} />,
-  },
-};
-
-export const ChildrenWithIcon = {
-  args: {
-    variant: "outline",
-    color: "success",
-    icon: <Monicon color="#40c057" name="lucide:check" size={15} />,
-    children: "Button",
-  },
-};
-
-export const DangerButton = {
-  args: {
-    color: "danger",
-    variant: "elevated",
-    children: "Delete Account",
-  },
+export const ButtonVariants = () => {
+  return (
+    <Stack gap={4}>
+      <strong>Primary</strong>
+      <Group gap={4}>
+        <Button>Button</Button>
+        <Button color="secondary">Button</Button>
+        <Button color="success">Button</Button>
+        <Button color="danger">Button</Button>
+      </Group>
+      <strong>Outline</strong>
+      <Group gap={4}>
+        <Button variant="outline">Button</Button>
+        <Button variant="outline" color="secondary">
+          Button
+        </Button>
+        <Button variant="outline" color="success">
+          Button
+        </Button>
+        <Button variant="outline" color="danger">
+          Button
+        </Button>
+      </Group>
+      <strong>Subtle</strong>
+      <Group gap={4}>
+        <Button variant="subtle">Button</Button>
+        <Button variant="subtle" color="secondary">
+          Button
+        </Button>
+        <Button variant="subtle" color="success">
+          Button
+        </Button>
+        <Button variant="subtle" color="danger">
+          Button
+        </Button>
+      </Group>
+      <strong>Buttons with Icon</strong>
+      <Group gap={4}>
+        <Button icon={<Monicon size={15} name="lucide:trash-2" />}>
+          Button
+        </Button>
+        <Button
+          icon={<Monicon size={15} name="lucide:trash-2" />}
+          variant="outline"
+          color="secondary"
+        >
+          Button
+        </Button>
+        <Button
+          icon={<Monicon size={15} name="lucide:trash-2" />}
+          variant="subtle"
+          color="danger"
+        >
+          Button
+        </Button>
+      </Group>
+      <strong>Icon Buttons</strong>
+      <Group gap={4}>
+        <Button icon={<Monicon size={15} name="lucide:trash-2" />} />
+        <Button
+          icon={<Monicon size={15} name="lucide:trash-2" />}
+          variant="outline"
+          color="secondary"
+        />
+        <Button
+          icon={<Monicon size={15} name="lucide:trash-2" />}
+          variant="subtle"
+          color="danger"
+        />
+      </Group>
+      <strong>Icon Buttons</strong>
+      <Group gap={4}>
+        <Button loading>Button</Button>
+        <Button loading variant="outline" color="secondary">
+          Button
+        </Button>
+        <Button loading variant="subtle" color="danger">
+          Button
+        </Button>
+        <Button loading variant="subtle" color="success">
+          Button
+        </Button>
+      </Group>
+    </Stack>
+  );
 };
 
 export default ButtonStory;
